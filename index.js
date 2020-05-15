5cb20f7d926e67a08b32e062a15c04a55705ff48
Ahmed Jamal <engahmedd96@gmai.com>
2020-05-15 20:12:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+main.remove();
+document.querySelector('body');
+let newHeader=document.createElement('h1');
+body.appendChild(newHeader);
+newHeader.setAttribute("id", "vectory");
+
+
+
+ 
+
